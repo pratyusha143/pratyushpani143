e90d123bcb3d9093c74eb2d664abf2618e2d3d03
HOME <HOME@DESKTOP-5SKJAR1>
2018-06-11 15:13:11 +0530

    cfftrgrf

--- proj1/src/test/proj1.java
+++ proj1/src/test/proj1.java
@@ -6,6 +6,7 @@ public class proj1 {
 	public static void main(String[] args) {
 	System.out.println("i am ready");
 System.out.println("developer ready");
+System.out.println("developer readyffwwqarwe");
 	}
 
 }
